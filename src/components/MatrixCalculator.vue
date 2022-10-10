151ed91cd72f58106ca6af8b0adcf3c05813660a
VasylHol <golovachko2018@gmail.com>
2022-10-10 19:55:38 +0300

    epsilon changes

--- src/components/MatrixCalculator.vue
+++ src/components/MatrixCalculator.vue
@@ -3,6 +3,9 @@
         <div class="size_field">
             <span>Введіть розмірність матриці:</span>
             <input type="number" name="size" id="size" v-model="size" />
+            <span>Введіть eps:</span>
+            <input type="number" name="eps" id="eps" v-model="eps" />
+
         </div>
         <div class="wrapper">
             <div>
@@ -19,10 +22,18 @@
                 </div>
             </div>
         </div>
-        <button @click="det(matrix)">Обчислити детермінант</button>
         <button @click="method_zeidel">Метод Зейделя</button>
         <button @click="method_yacobi">Метод Якобі</button>
-        <div>{{determinant}}</div>
+
+
+        <div class="history">
+            <div v-for="(el, index) in history" :key="index">
+                N ={{index+1}}
+                <div v-for="(element, indx) in el" :key="indx + 24" style="margin-right:15px">
+                    x{{indx+1}}={{element}}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
   
@@ -31,29 +42,9 @@ import { ref, watch } from 'vue';
 
 const size = ref(3)
 let matrix = ref([])
-const determinant = ref(0)
 let eps = ref(0.0001)
+let history = ref([])
 let vector = ref([])
-function det(M) {
-    if (M.length == 2) {
-        determinant.value = (M[0][0] * M[1][1]) - (M[0][1] * M[1][0])
-        return (M[0][0] * M[1][1]) - (M[0][1] * M[1][0]);
-    }
-    let answer = 0;
-    for (var i = 0; i < M.length; i++) {
-        answer += Math.pow(-1, i) * M[0][i] * det(deleteRowAndColumn(M, i));
-    }
-    determinant.value = answer;
-}
-
-function deleteRowAndColumn(M, index) {
-    var temp = [];
-
-    for (var i = 0; i < M.length; i++) { temp.push(M[i].slice(0)); }
-    temp.splice(0, 1);
-    for (i = 0; i < temp.length; i++) { temp[i].splice(index, 1); }
-    return temp;
-}
 
 watch(size, () => {
     let arr = [];
@@ -72,35 +63,37 @@ watch(size, () => {
 
 
 function method_zeidel() {
+    history.value = []
     let x_vector = new Array(size.value).fill(0)
     const el = {}
     let converge = false
     let counter = 0
     while (!converge) {
-        let x_old = Object.assign({}, x_vector)
+        let x_old;
+        if (counter == 0) {
+            x_old = new Array(size.value).fill(0)
+        }
+        else {
+            x_old = Object.assign([], x_vector)
+        }
         for (let i = 0; i < size.value; i++) {
             let x_new = Object.assign({}, x_vector)
             x_new[i] = 1
             el[`s${i}`] = matrix.value[i].map((element) => element * -1)
             el[`s${i}`][i] = vector.value[i]
-            console.log(el[`s${i}`])
             el[`s${i}`] = el[`s${i}`].map((element) => element / matrix.value[i][i])
-            console.log(vector.value)
-            console.log(el[`s${i}`])
             el[`s${i}_sum`] = sum_array(el[`s${i}`].map((element, index) => element * x_new[index]))
             x_vector[i] = el[`s${i}_sum`].toFixed(3)
-            console.log(el[`s${i}_sum`])
         }
-        converge = Array.from(x_old).map((element, index) => {
-            return Math.abs(element - x_vector[index]) < eps.value
-        })
-        console.log(counter, "COUNTER")
+        history.value.push(Object.assign([], x_vector))
+        converge = Math.abs(sum_array(x_vector) - sum_array(x_old)) <= eps.value
+        counter++
     }
 }
 function sum_array(arr) {
     let summ = 0
     for (let i = 0; i < arr.length; i++) {
-        summ += arr[i]
+        summ += Number(arr[i])
     }
     return summ
 }
@@ -124,11 +117,11 @@ function method_yacobi() {
             test[i] = el[`s${i}_sum`].toFixed(3)
             console.log(el[`s${i}_sum`])
         }
+        converge = Math.abs(sum_array(x_vector) - sum_array(test)) <= eps.value
         x_vector = test
+        history.value.push(x_vector)
         counter = counter + 1
-        if (counter == 2) {
-            converge = true
-        }
+
         console.log(counter, "COUNTER")
     }
 }
@@ -143,6 +136,10 @@ function method_yacobi() {
 .wrapper {
     display: flex;
     height: 200px;
+}
+
+.history {
+    display: flex;
 }
 
 .cell {
